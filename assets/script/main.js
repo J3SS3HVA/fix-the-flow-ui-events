9bc5876f319f8b7ad9588fb1049d0ede06c1a2a9
J3SS3HVA <jesse.leeuwerink@hva.nl>
2023-12-11 13:04:21 +0100

    10 events gemaakt

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,96 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
+let mouse_over = document.querySelector('a:nth-of-type(1)') 
+let doubleclick = document.querySelector('a:nth-of-type(2)')
+let mouse_down = document.querySelector('a:nth-of-type(3)')
+let mouse_up = document.querySelector('a:nth-of-type(4)')
+let key_down = document.querySelector('a:nth-of-type(5)')
+let key_up = document.querySelector('a:nth-of-type(6)')
+let key_press = document.querySelector('a:nth-of-type(7)')
+let mouse_move = document.querySelector('a:nth-of-type(8)')
+let scroll_ = document.querySelector('a:nth-of-type(9)')
+let focus_ = document.querySelector('a:nth-of-type(10)')
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
+}
+
+mouse_over.addEventListener('mouseover', lightgreen)
+
+function lightgreen() {
+  mouse_over.classList.toggle('MOUSEOVER')
+}
+
+doubleclick.addEventListener('dblclick', font_fantasy)
+
+function font_fantasy() {
+  doubleclick.classList.toggle('DUBBELCLICK')
+}
+
+mouse_down.addEventListener('mousedown', texttransform)
+
+function texttransform() {
+  mouse_down.classList.toggle('MOUSEDOWN')
+}
+
+mouse_up.addEventListener('mouseup', texttransformremove)
+
+function texttransformremove() {
+  mouse_up.classList.toggle('MOUSEUP')
+}
+
+document.addEventListener("keydown", ptoets);
+
+function ptoets(e) {
+	if (e.key =="p") {
+		key_down.classList.toggle('KEYDOWN')
+	}
+}
+
+document.addEventListener("keyup", ftoets);
+
+function ftoets(e) {
+	if (e.key =="f") {
+		key_up.classList.toggle('KEYUP')
+	}
+}
+
+document.addEventListener("keypress", stoets);
+
+function stoets(e) {
+	if (e.key =="s") {
+		key_press.classList.toggle('KEYPRESS')
+	}
+}
+
+mouse_move.addEventListener('mousemove', randomcolor)
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function randomcolor() {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  mouse_move.style.backgroundColor = rndCol;
+  console.log('randomcolor')
+}
+
+scroll_.addEventListener('wheel', randomcolor1)
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function randomcolor1() {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  scroll_.style.backgroundColor = rndCol;
+  console.log('randomcolor1')
+}
+
+focus_.addEventListener('focus', giant)
+
+function giant() {
+  focus_.classList.toggle('FOCUS')
 }
